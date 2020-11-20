12dfaa207baf876173bd408d41292b1e261841d9
Pc <gabweglarz@gmail.com>
2020-11-20 13:57:11 +0100

    Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+        String str = "kajak";
+        if(Palindrom(str)){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
+    }
+    static boolean Palindrom(String str){
+        int i = 0;
+        int j = str.length() - 1;
+        while(i < j){
+            if(str.charAt(i) != str.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
